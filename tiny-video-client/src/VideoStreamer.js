d55141a96e21deec74d5d903ee938502b1fbc015
Photis Paschos <photis.paschos@hellasdirect.gr>
2019-09-25 18:15:27 +0300

    Getting access to webrowser  camera.

--- tiny-video-client/src/VideoStreamer.js
+++ tiny-video-client/src/VideoStreamer.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import 'webrtc-adapter';
 
 export default class VideoStreamer extends React.Component {
 
     constructor(props) {
         super(props);
+        this.videoRef = null;
         this.state = {
-            test: false
+            cameraAvailable: false
         }
     }
 
     componentDidMount() {
-        navigator.mediaDevices.getUserMedia({video: {width: 426, height: 240} })
+        navigator.mediaDevices
+            .getUserMedia({video: true})
+            .then((stream) => {
+                this.videoRef.srcObject = stream;
+
+                this.setState({cameraAvailable: true})
+                // this.videoRef.start();
+            })
+            .catch(e => {
+                console.error(e.name + ": " + e.message)
+            })
     }
 
     render() {
+
+        const style = this.state.cameraAvailable ? { display: 'none'} : {};
         return (
-            <p> Video stream goes here!</p>
+            <div className="camera">
+                <video autoPlay
+                       style={{style}}
+                       ref = {ref => this.videoRef = ref}>
+                    <p>Video stream not available.</p>
+                </video>
+                {!this.state.cameraAvailable && <p>Camera not available!</p>}
+            </div>
         )
     }
 }
